a9d7ccf9fdf53690435f5a7854320922692a4772
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-16 17:34:29 +0100

    Update ParallelGPR.py

--- ParallelGPR.py
+++ ParallelGPR.py
@@ -67,8 +67,17 @@ class ParallelGPR:
     def load(self, name):
         self.mod, self.w = load(f"{name}.joblib")
         
-        
- def fitParallelGPR(name, number_models = 40, name_model = "Parallel", save = True):
+  
+def fitParallelGPR(name, number_models = 40, name_model = "Parallel", save = True):
+    """
+    Function to fit a Parallel Gaussian Process Regressor given a Pandas dataframe containing in the first column 
+    the price to learn, and in the remaining columns the feature used to learn the pricing function 
+    - name: string variable describing the name of the dataset to use as training sample. 
+    - number_models: int variable that specifies how many models need to be fitted. 
+    - save: boolean variable that describes if the model need to be fitted
+    - name_model: string variable describing the how the model need to be saved, to use without .joblib extension.
+                  needed only when save = True
+    """
     df = pd.read_csv(name).to_numpy()[:,1:]
     X = df[:,1:]
     y = df[:,0]
