67d8cda01c5f6f1b62e3dd075fba89e8a5a66288
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-20 19:55:33 +0100

    Update optim.py

--- optim.py
+++ optim.py
@@ -1,11 +1,21 @@
 import scipy
 import scipy.optimize
 
+def optim(self, obj_func, initial_theta, bounds):
+        """
+        Inputs:
+            obj_func : function
+                     function to minimize
+            initial_theta : float or list of float
+            bounds : list of tuples
+        Outputs:
+            theta_opt : float
+            func_min : float
+        """
 
-def optim(obj_func, initial_theta, bounds):
-    optimResult = scipy.optimize.minimize(
-        obj_func, initial_theta, method="COBYLA", jac=False
-    )
-    theta_opt = optimResult.x
-    func_min = optimResult.fun
-    return theta_opt, func_min
+        optimResult = scipy.optimize.minimize(
+            obj_func, initial_theta, method="COBYLA", jac=False
+        )
+        theta_opt = optimResult.x
+        func_min = optimResult.fun
+        return theta_opt, func_min
