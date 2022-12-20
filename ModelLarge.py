139fcdb81d7e66e256709bd84adf3c66fbd8adbe
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-20 10:19:14 +0100

    Update ModelLarge.py

--- ModelLarge.py
+++ ModelLarge.py
@@ -3,7 +3,21 @@ import torch.nn as nn
 
 
 class ModelLarge(nn.Module):
+    """
+    
+    class to define the model large 
+    
+    """
     def __init__(self, n_init, hidden_dim):
+        """
+        Inputs: 
+        
+            n_init : int
+                    number of feature of the X matrix 
+            
+            hidden_dim : int 
+        
+        """
         super().__init__()
         self.model = nn.Sequential(
             nn.Linear(n_init, hidden_dim),
