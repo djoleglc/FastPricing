fe46b10b05f6d8b881993c2e980194ff3106c55f
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-20 19:45:53 +0100

    Update ParallelGPR.py

--- ParallelGPR.py
+++ ParallelGPR.py
@@ -5,17 +5,48 @@ from joblib import dump, load
 import joblib
 from joblib import Parallel, delayed
 import scipy
-import math 
+import math
 import pandas as pd
-import time 
 
 
 class ParallelGPR:
+    """
+
+    class that defines a ParallelGPR object
+
+    """
+
     def __init__(self, N_Model):
+        """
+
+        Inputs:
+
+            N_model : int
+                    number of models to fit
+
+        """
         self.N_Model = N_Model
-        
 
     def optim(self, obj_func, initial_theta, bounds):
+        """
+        Inputs:
+
+            obj_func : function
+                     function to minimize
+
+            initial_theta : float or list of float
+
+            bounds : list of tuples
+
+        Outputs:
+
+            theta_opt : float
+
+            func_min : float
+
+
+        """
+
         optimResult = scipy.optimize.minimize(
             obj_func, initial_theta, method="COBYLA", jac=False
         )
@@ -24,6 +55,19 @@ class ParallelGPR:
         return theta_opt, func_min
 
     def functionGPR(self, xy):
+        """
+
+        Inputs:
+
+            xy : list or tuple of numpy array
+                tuple or list containing the y array and the X matrix
+
+        Output:
+
+            gprM : sklearn model
+
+
+        """
         x, y = xy
         kernel = k.RBF()
         gprM = GaussianProcessRegressor(
@@ -32,15 +76,54 @@ class ParallelGPR:
         return gprM
 
     def predictGPR(self, xm):
+        """
+        Inputs:
+
+            xy : list or tuple of numpy array
+                tuple or list containing the x matrix and a sklearn model
+
+        Output:
+
+            m.predict(x) : numpy array
+                         array of predicted values computed usinf the model
+
+        """
         x, m = xm
         return m.predict(x)
 
     def GetWeight(self, y, X):
+        """
+        Inputs:
+
+            y : numpy array
+               contains the true values
+
+            X : numpy array
+               contains the predicted values obtained through a model
+
+
+        Outputs:
+
+            w : numpy array
+               weight calculated on MAE loss
+
+        """
         MAE = X - y.reshape(-1, 1)
         mae_mean = np.mean(MAE, 0) ** (-1)
-        return (mae_mean / mae_mean.sum()).reshape(-1, 1)
+        w = (mae_mean / mae_mean.sum()).reshape(-1, 1)
+        return w
 
     def fit(self, X, y):
+        """
+        Inputs:
+
+            X : numpy array
+              matrix containing the features
+
+            y : numpy array
+              array containing the true prices
+
+        """
         Xs = np.split(X, self.N_Model)
         ys = np.split(y, self.N_Model)
         # fitting the models
@@ -57,38 +140,82 @@ class ParallelGPR:
         self.w = self.GetWeight(y, predicted)
 
     def predict(self, X):
+        """
+        Inputs:
+
+            X : numpy array
+              matrix containing feautures corresponding to the price to predict
+
+        Outputs:
+
+            pred_w : numpy array
+                    predicted prices
+
+        """
         pred = Parallel(n_jobs=-2)(delayed(self.predictGPR)([X, m]) for m in self.mod)
         pred = np.array(pred).T
-        return pred @ self.w
+        pred_w = pred @ self.w
+        return pred_w
 
     def save(self, name):
+        """
+        Inputs:
+
+            name : str
+                  name of the file to save
+
+        """
         tosave = [self.mod, self.w]
         dump(tosave, f"{name}.joblib")
 
     def load(self, name):
-        self.mod, self.w = load(f"{name}.joblib")
-        
+        """
+        Inputs:
 
-def fitParallelGPR(name, number_models = 40, name_model = "Parallel", save = True, time_ = True):
+            name : str
+                 name of the file to load
+
+        """
+        self.mod, self.w = load(f"{name}.joblib")
+
+
+def fitParallelGPR(
+    name, number_models=40, name_model="Parallel", save=True, time_=True
+):
     """
-    Function to fit a Parallel Gaussian Process Regressor given a Pandas dataframe containing in the first column 
-    the price to learn, and in the remaining columns the feature used to learn the pricing function 
-    - name: string variable describing the name of the dataset to use as training sample. 
-    - number_models: int variable that specifies how many models need to be fitted. 
-    - save: boolean variable that describes if the model need to be fitted
-    - name_model: string variable describing the how the model need to be saved, to use without .joblib extension.
-                  needed only when save = True
-    -time_ : boolean variable describing if it needed to return the fitting time 
+    Function to fit a Parallel Gaussian Process Regressor given a Pandas dataframe containing in the first column
+    the price to learn, and in the remaining columns the feature used to learn the pricing function
+
+    Inputs:
+
+        name : str
+             variable describing the name of the dataset to use as training sample.
+
+        number_models : int
+                      variable that specifies how many models need to be fitted.
+
+        save : bool
+             variable that describes if the model need to be fitted
+
+
+        name_model : str
+                    variable describing the how the model need to be saved, to use without .joblib extension.
+                    needed only when save = True
+
+    Outputs:
+
+        m : ParalleGPR
+
     """
-    df = pd.read_csv(name).to_numpy()[:,1:]
-    X = df[:,1:]
-    y = df[:,0]
+    df = pd.read_csv(name).to_numpy()[:, 1:]
+    X = df[:, 1:]
+    y = df[:, 0]
     m = ParallelGPR(number_models)
     s = time.time()
-    m.fit(X,y)
+    m.fit(X, y)
     e = time.time()
     if time_:
-        print(f"Fitting Time:  {e-s}\n")
+        print(f"Time:  {e-s}")
     if save:
         m.save(name_model)
-    return m 
+    return m
