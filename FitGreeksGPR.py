0bc8c97f2f0c768091096ed8dc31375d72f2fad8
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-20 19:42:38 +0100

    Update FitGreeksGPR.py

--- FitGreeksGPR.py
+++ FitGreeksGPR.py
@@ -4,6 +4,8 @@ from joblib import dump
 import numpy as np
 import joblib
 import multiprocessing
+from TestPerformance import test_performanceGPR
+
 
 def fitGreeksGPR(
     greek_df, price_df, model, name_model=None, save=False, time_=True, restart=2
@@ -11,15 +13,32 @@ def fitGreeksGPR(
     """
     Function to fit a Parallel Gaussian Process Regressor given a Pandas dataframe containing in the first column
     the price to learn, and in the remaining columns the feature used to learn the pricing function
-    Note that both greeks ans prices dataframe need to be pandas dataframe 
-    
-    - greek_df: string variable describing the name of the csv containing the greeks to use to help 
-                the training phase ( in the following order "theta", "delta", "vega", "vegalt", "rho" )
-    - price_df: string variable describing the name of the csv containing prices (first columns) and features
-    - save: boolean variable that describes if the model need to be fitted
-    - name_model: string variable describing the how the model need to be saved, to use without .joblib extension.
-                  needed only when save = True
-    -time_ : boolean variable describing if it needed to return the fitting time
+    Note that both greeks ans prices dataframe need to be pandas dataframe
+
+    Inputs:
+
+        greek_df : string variable describing the name of the csv containing the greeks to use to help
+                 the training phase ( in the following order "theta", "delta", "vega", "vegalt", "rho" )
+
+        price_df : str
+                 variable describing the name of the csv containing prices (first columns) and features
+
+        model : GreeksGPR model
+
+        save : bool
+              variable that describes if the model need to be fitted
+
+        name_model : str
+                   variable describing the how the model need to be saved, to use without .joblib extension.
+                   needed only when save = True
+
+       time_ : bool
+             variable describing if it needed to return the fitting time
+
+    Output:
+
+        self : GreeksGPR model
+
     """
     greeks = pd.read_csv(greek_df, header=None)
     greeks.columns = ["theta", "delta", "vega", "vegalt", "rho"]
@@ -33,7 +52,7 @@ def fitGreeksGPR(
 
     df = pd.read_csv(price_df).to_numpy()[:, 1:]
     X = df[:, 1:]
-    price, greek_variable = df[:, 0],  greek_variable.to_numpy()
+    price, greek_variable = df[:, 0], greek_variable.to_numpy()
 
     if model.__class__.__name__ == "DeltaGPR":
         s = time.time()
@@ -49,3 +68,74 @@ def fitGreeksGPR(
     if save:
         dump(model, name_model)
     return model
+
+
+class cv_alpha_GreeksGPR:
+    """
+
+    Class that define an validation procedure
+
+    """
+
+    def __init__(self):
+        self.mae = []
+        self.aae = []
+        self.bestMAE = None
+        self.bestAAE = None
+
+    def fit(self, alphas, class_, greeks_dataset, price_dataset, validation_dataset):
+        """
+        Function to fit the model for each alpha
+
+        Inputs:
+
+            alphas : list or numpy array
+                    contains the values of alpha to use in the model
+
+            class_ : class
+                    class of a GreeksGPR model
+
+            greeks_dataset : pd.DataFrame
+
+            price_dataset : pd.DataFrame
+
+            validation_dataset : pd.DataFrame
+
+        Output:
+
+            cv_alpha_GreeksGPR object
+
+        """
+
+        for i, alpha_ in enumerate(alphas):
+            mod = class_(alpha=alpha_)
+            mod = fitGreeksGPR(
+                greek_df=greeks_dataset,
+                price_df=price_dataset,
+                model=mod,
+                save=False,
+                time_=False,
+                restart=2,
+            )
+
+            # performance of the model
+            print(f"alpha:   {alpha_}")
+            print(validation_dataset)
+            df = pd.read_csv(validation_dataset).to_numpy()[:, 1:]
+            X = df[:, 1:]
+            y = df[:, 0]
+            aae_, mae_ = test_performanceGPR(X, y, mod, type_="both", to_return=True)
+            self.aae.append(aae_)
+            self.mae.append(mae_)
+
+            if i > 0:
+                if mae_ <= np.min(self.mae):
+                    self.bestMAE = mod
+                if aae_ <= np.min(self.aae):
+                    self.bestAAE = mod
+            else:
+                self.bestMAE = mod
+                self.bestAAE = mod
+            print("\n")
+
+        return self
