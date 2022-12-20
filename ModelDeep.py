acce5fb9ba4ac846b43c2a314d645c67aebb3283
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-12-20 10:21:24 +0100

    Update ModelDeep.py

--- ModelDeep.py
+++ ModelDeep.py
@@ -3,7 +3,21 @@ import torch.nn as nn
 
 
 class ModelDeep(nn.Module):
+    """
+    
+    class to define the model deep 
+    
+    """
     def __init__(self, n_init, hidden_dim):
+        """
+        Inputs: 
+        
+            n_init : int
+                    number of feature of the X matrix 
+            
+            hidden_dim : int 
+        
+        """
         super().__init__()
         self.model = nn.Sequential(
             nn.Linear(n_init, hidden_dim),
@@ -27,5 +41,12 @@ class ModelDeep(nn.Module):
         )
 
     def forward(self, x):
+        """
+        Inputs : 
+        
+            x : torch tensor 
+                input for which we need to estimate the price through the model 
+        
+        """
         output = self.model(x)
         return output
